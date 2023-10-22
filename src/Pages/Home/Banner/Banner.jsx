625d13440dc2d2a8e6cf23121e000d1e8c7339b3
arifu00 <arifu5758@gmail.com>
2023-10-22 23:18:19 +0600

    added navbar footer & banner section

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,208 @@
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
+const Banner = () => {
+  return (
+    <div>
+      <div className="carousel h-[600px] w-full my-5">
+        {/* slide-1  */}
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12 ">
+            <a href="#slide6" className="btn btn-circle">
+              ❮
+            </a>
+            <a
+              href="#slide2"
+              className="btn btn-circle  bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* slide-2  */}
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a
+              href="#slide3"
+              className="btn btn-circle  bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* slide-3  */}
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a
+              href="#slide4"
+              className="btn btn-circle  bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* slide-4  */}
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a
+              href="#slide5"
+              className="btn btn-circle  bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* slide-5  */}
+        <div id="slide5" className="carousel-item relative w-full">
+          <img src={img5} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12">
+            <a href="#slide4" className="btn btn-circle ">
+              ❮
+            </a>
+            <a
+              href="#slide6"
+              className="btn btn-circle  bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* slide-6  */}
+        <div id="slide6" className="carousel-item relative w-full">
+          <img src={img6} className="w-full" />
+          <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0.00)  ">
+            <div className="text-white w-1/2 pl-8 space-y-7 md:pl-20">
+              <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                Affordable Price For Car Servicing
+              </h2>
+              <p className="text-lg font-normal">
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-[#FF3811] hover:bg-[#50C1EC] mr-5">
+                Discover More
+              </button>
+              <button className="text-lg font-semibold px-4 py-3 rounded-md bg-transparent outline-white outline hover:bg-[#FF3811] ">
+                Latest Project
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 bottom-0 gap-5 right-12">
+            <a href="#slide5" className="btn btn-circle ">
+              ❮
+            </a>
+            <a
+              href="#slide1"
+              className="btn btn-circle bg-[#FF3811] text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/SharedStyle/NavBar/NavBar.jsx
@@ -0,0 +1,103 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+const NavBar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-pink-600 underline" : ""
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/about"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-pink-600 underline" : ""
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/services"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-pink-600 underline" : ""
+          }
+        >
+          Services
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/blog"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-pink-600 underline" : ""
+          }
+        >
+          Blog
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-pink-600 underline" : ""
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-lg font-medium"
+            >
+              {navLinks}
+            </ul>
+          </div>
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-lg font-medium">{navLinks}</ul>
+        </div>
+        <div className="navbar-end">
+          <NavLink to="">
+            <button className="btn btn-outline btn-primary">Appointment</button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
